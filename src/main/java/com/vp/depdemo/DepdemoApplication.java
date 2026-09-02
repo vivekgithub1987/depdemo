9e6dea091b3f0c0fcb8ee4adc7741e687b73f8a9
Vivekanand <vivek.palamkar@gmail.com>
2026-09-02 14:41:52 +0530

    Initial commit

---
+++ src/main/java/com/vp/depdemo/DepdemoApplication.java
@@ -0,0 +1,13 @@
+package com.vp.depdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vp/depdemo/DepdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vp.depdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
